241b4faf76c9a9d9434df2148d4bf555b933fd74
kanamana <80589804+kanamana@users.noreply.github.com>
2021-03-13 21:48:01 +0600

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(5)
+
+while True:
+  conn, adrr = s.accept()
+  while True:
+    data = conn.recv(1024)
+    if not data or data == 'close': break
+    conn.send(data)
+  conn.close()
